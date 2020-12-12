5f6793b8dab87ebdfe0e2a3542c3a4dde65a614a
LewRit <lewrit16@gmail.com>
2020-12-12 11:55:04 -0500

    added cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import Pokecard from './components/pokecard'
+import { Switch, Route, Link } from 'react-router-dom'
+import {Home, ContactUs, AboutUs, Pokecard} from './components'
 
 function App() {
   
   return (
-    <div>This is the webpage
-    <Pokecard  />
+    <div>
+      <header>
+      <Link to="/">Home</Link>
+        <div className="navs">
+          <Link to="/pokecard">Play Pokecard</Link>
+          <Link to="/about-us">About Us</Link>
+          <Link to="/contact-us">Contact Us</Link>
+        </div>
+      </header>
+      <main>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/pokecard' component={Pokecard}/>
+          <Route exact path= '/contact-us' component={ContactUs}/>
+          <Route exact path='/about-us' component={AboutUs}/>
+        </Switch>
+      </main>
+      <footer>
+        This is footer
+      </footer>
     </div>
   );
 }
