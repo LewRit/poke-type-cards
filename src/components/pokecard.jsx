2f5a6f72c4fd44d276e90b19059653fb844df96a
LewRit <lewrit16@gmail.com>
2020-12-12 14:07:43 -0500

    card populated

--- src/components/pokecard.jsx
+++ src/components/pokecard.jsx
@@ -1,33 +1,24 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import data from './typeCard'
 
 const Pokecard =()=>{
+    const [selected,setSelected]=useState()
+    const Card=(props)=>{
         return(
-            <div className='pokemon'>
-                <div className="card" style={{width: '18rem'}}>
-                    <img src="https://www.pokepedia.fr/images/thumb/8/8f/Ponyta-RFVF.png/250px-Ponyta-RFVF.png" className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Fire</h5>
-                        <p className="card-text">Fire Pony</p>
-                        <a href="#" className="btn btn-primary">Has advantage against</a>
-                    </div>
-                </div>
-                <div className="card" style={{width: '18rem'}}>
-                    <img src="https://pm1.narvii.com/6778/329fd36057877fc1f90d1840c52baeea14e620b8v2_hq.jpg" className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Grass</h5>
-                        <p className="card-text">Bulbasaur</p>
-                        <a href="#" className="btn btn-primary">Has advantage against</a>
-                    </div>
-                </div>
-                <div className="card" style={{width: '18rem'}}>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFUm-yBI1IpYJXHQJFfmlhH_F-x8FPjupWgQ&usqp=CAU" className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Water</h5>
-                        <p className="card-text">Squirtle</p>
-                        <a href="#" className="btn btn-primary">Has advantage against</a>
-                    </div>
+            <div className="card" style={{width: '18rem'}}>
+                <img src={props.sprite} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{props.type}</h5>
+                    <p className="card-text">{props.name}</p>
+                    <a href="#" className="btn btn-primary">Has advantage against</a>
                 </div>
             </div>
         )
+    }
+    return(
+        <div className='pokemon'>
+            {data.map((card,index)=><Card  key={index} sprite={card.sprite} type={card.type} name={card.name}/>)}
+        </div>
+    )
 }
 export default Pokecard;
