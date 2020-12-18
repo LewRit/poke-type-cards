7053d09b513137535657b4522d93a28f2a62542d
LewRit <lewrit16@gmail.com>
2020-12-17 22:00:31 -0500

    hooked redux

--- src/redux/reducers/createGame.js
+++ src/redux/reducers/createGame.js
@@ -1,5 +1,18 @@
 import {CREATE_GAME} from '../actionTypes'
+import data from '../../components/typeCard'
 
 const initialState={
-    
+    board:[]    //set to 8 cards after it works.
+}
+export default function(state=initialState,action){
+    switch(action.type){
+        case CREATE_GAME:{
+            return{
+                ...state,
+                board:data.slice(0,8) //gives me first 8 cards
+            }
+        }
+        default:
+            return state;
+    }
 }
